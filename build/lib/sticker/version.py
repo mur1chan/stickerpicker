ac2a9733ee1f53b94f046e200bf8ed8a87edc01d
mur1chan <muerchen@hotmail.com>
2024-09-26 23:31:55 +0200

    cinamoroll, memes

--- build/lib/sticker/version.py
+++ build/lib/sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = '47f17fde'
-version = '0.1.0+dev.47f17fde'
-linkified_version = '0.1.0+dev.[47f17fde](https://github.com/maunium/stickerpicker/commit/47f17fde452b5e9f0c9e96ce0e2c878dd0574b7f)'
+git_revision = '20750a38'
+version = '0.1.0+dev.20750a38'
+linkified_version = '0.1.0+dev.[20750a38](https://github.com/maunium/stickerpicker/commit/20750a3808ceb84efa41ba61c00edc77f3a8e7de)'
